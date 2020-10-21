a6d97395f61ca9cfe5c80ee7452707f5bc2a97dd
Billkirk123 <billykirk1997@gmail.com>
2020-10-21 09:18:56 +0100

    scroller added
    Scroller with placeholder cards working

---
+++ UI_Test/Assets/scripts/scroller.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using TMPro;
+using SimpleJSON;
+public class scroller : MonoBehaviour
+{
+    public ScrollRect myScrRect;
+
+    public GameObject cardPrefab;
+
+    public GameObject content;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            Instantiate(cardPrefab, content.transform);
+        }
+
+        myScrRect.verticalNormalizedPosition = 1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (myScrRect.verticalNormalizedPosition <= 0.01f)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                Instantiate(cardPrefab, content.transform);
+            }
+
+            myScrRect.verticalNormalizedPosition = 0.05f;
+        }
+    }
+}
