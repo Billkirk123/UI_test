6dd552c1995d8ada22ec102fc3e067cf6aa5aed5
Billkirk123 <billykirk1997@gmail.com>
2020-10-22 16:19:59 +0100

    3rd page done

---
+++ UI_Test/Assets/scripts/ScrollSnap.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollSnap : MonoBehaviour
+{
+    public RectTransform panel;
+    public RawImage[] element;
+    public RectTransform center;
+
+    private float[] distance;
+    private bool dragging = false;
+    private int elementDistance;
+    private int minElementNum;
+
+
+    //card sizes
+    Vector3 defultSize = new Vector3(2f, 2f, 2f);
+    Vector3 size1 = new Vector3(0.75f, 0.75f, 1f);
+    Vector3 size2 = new Vector3(0.5f, 0.5f, 0.5f);
+    Vector3 size3 = new Vector3(0.25f, 0.25f, 0.25f);
+
+    
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int elementLength = element.Length;
+        distance = new float[elementLength];       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i = 0; i < element.Length; i++)
+        {
+            //get distance between all cards and center point
+            distance[i] = Mathf.Abs(center.transform.position.x - element[i].transform.position.x);
+
+            //change card size and position depending on how far away from the centre they are
+            //Quite messy honestly
+            if (distance[i] < 200)
+            {
+                element[i].transform.localScale = Vector3.Lerp(element[i].transform.localScale, defultSize, 0.1f);
+                element[i].rectTransform.anchoredPosition = Vector2.MoveTowards(element[i].rectTransform.anchoredPosition, new Vector2(element[i].rectTransform.anchoredPosition.x, 85), 5f);
+            }
+            else if (distance[i] >= 200 && distance[i] < 400)
+            {
+                element[i].transform.localScale = Vector3.Lerp(element[i].transform.localScale, size1, 0.1f);
+                element[i].rectTransform.anchoredPosition = Vector2.MoveTowards(element[i].rectTransform.anchoredPosition, new Vector2(element[i].rectTransform.anchoredPosition.x, -50), 5f);
+            }
+            else if (distance[i] >= 400 && distance[i] < 600)
+            {
+                element[i].transform.localScale = Vector3.Lerp(element[i].transform.localScale, size2, 0.1f);
+                element[i].rectTransform.anchoredPosition = Vector2.MoveTowards(element[i].rectTransform.anchoredPosition, new Vector2(element[i].rectTransform.anchoredPosition.x, -115), 5f);
+            }
+            else if (distance[i] >= 600)
+            {
+                element[i].transform.localScale = Vector3.Lerp(element[i].transform.localScale, size3, 0.1f);
+                element[i].rectTransform.anchoredPosition = Vector2.MoveTowards(element[i].rectTransform.anchoredPosition, new Vector2(element[i].rectTransform.anchoredPosition.x, -215), 5f);
+            }
+
+
+
+        }
+
+        //workout which card is closest to center
+        float minDistance = Mathf.Min(distance);
+
+        for (int a = 0; a < element.Length; a++)
+        {
+            if(minDistance == distance[a])
+            {
+                minElementNum = a;
+            }
+        }
+        if (!dragging)
+        {
+            lerpToElement(element[minElementNum].rectTransform.anchoredPosition.x*-1);
+        }
+    }
+
+    void lerpToElement(float image)
+    {
+        
+        Vector2 newPos = new Vector2(image, panel.anchoredPosition.y);
+
+        panel.anchoredPosition = Vector2.MoveTowards(panel.anchoredPosition, newPos, 10f);
+    }
+
+    //these use event handlers in the editor
+    public void StartDrag()
+    {
+        dragging = true;
+    }
+
+    public void endDrag()
+    {
+        dragging = false;
+    }
+
+
+}
+
+
+
+
--- UI_Test/Assets/scripts/scroller.cs
+++ UI_Test/Assets/scripts/scroller.cs
@@ -17,6 +17,7 @@ public class scroller : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //loading initial cards
         for (int i = 0; i < 10; i++)
         {
             Instantiate(cardPrefab2, content.transform);
@@ -30,7 +31,7 @@ public class scroller : MonoBehaviour
     void Update()
     {
 
-
+        //checks position of scroller, if near bottom it will load more cards, then moves the scroller up a little to stop it from loading too many
         if (myScrRect.verticalNormalizedPosition <= 0.01f)
         {
             for (int i = 0; i < 10; i++)
